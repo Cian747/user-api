49e3478130dfaee0340907c0a40527951a990e0c
Cian747 <studiokaiser1@gmail.com>
2025-05-21 10:38:12 +0300

    setting up user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
 
+MIN_LENGTH=8
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True,
+        min_legth=MIN_LENGTH
+    )
+
+    class Meta:
+        model = User
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework import status,viewsets,serializers
 from .models import Task
 from .serializers import TaskSerializer
+from django.contrib.auth.models import User
 
 @api_view(['GET', 'POST'])
 def task_list(request):
